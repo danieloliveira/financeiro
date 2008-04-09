2d33b7c1ea843bd8fdda89ce33962736c3be5afe
Rodrigo Urubatan Ferreira Jardim <rodrigo@urubatan.com.br>
2008-04-09 11:59:11 -0300

    initial schema definition

--- db/migrate/001_initial_models.rb
+++ db/migrate/001_initial_models.rb
@@ -1,13 +1,31 @@
 class InitialModels < ActiveRecord::Migration
   def self.up
     create_table :empresas do |t|
-      
+      t.string :nome, :null => false, :unique => true, :length => 120
+      t.timestamps!
     end
     create_table :usuarios do |t|
+      t.string :nome, :null => false, :unique => true, :length => 120
+      t.string :senha, :null => false, :length => 120
+      t.string :email, :null => false, :length => 250
+      t.timestamps!
     end
-    create_table :direito_gerenciamentos do |t|
-      t.belongs_to :usuarios
-      t.belongs_to :empresass
+    create_table :usuario_empresas do |t|
+      t.belongs_to :usuario, :null => false
+      t.belongs_to :empresa, :null => false
+      t.timestamps!
+    end
+    create_table :perfis do |t|
+      t.string :nome, :null => false, :unique => true, :length => 30
+      t.timestamps!
+    end
+    create_table :usuario_perfis do |t|
+      t.belongs_to :usuario, :null => false
+      t.belongs_to :empresa, :null => false
+      t.timestamps!
+    end
+    create_table :cliente do |t|
+      t.string :nome, :null => false
     end
   end
 
