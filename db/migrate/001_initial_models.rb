772aa630b4f7595dfa62c0c5c1588badcf8b4043
Rodrigo Urubatan <rodrigo@urubatan.com.br>
2008-04-10 00:06:45 -0300

    more fields for initial tables

--- db/migrate/001_initial_models.rb
+++ db/migrate/001_initial_models.rb
@@ -2,35 +2,42 @@ class InitialModels < ActiveRecord::Migration
   def self.up
     create_table :empresas do |t|
       t.string :nome, :null => false, :unique => true, :length => 120
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :usuarios do |t|
       t.string :nome, :null => false, :unique => true, :length => 120
       t.string :senha, :null => false, :length => 120
       t.string :email, :null => false, :length => 250
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :usuario_empresas do |t|
       t.belongs_to :usuario, :null => false
       t.belongs_to :empresa, :null => false
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :perfis do |t|
       t.string :nome, :null => false, :unique => true, :length => 30
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :usuario_perfis do |t|
       t.belongs_to :usuario, :null => false
       t.belongs_to :empresa, :null => false
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :clientes do |t|
       t.string :nome, :null => false
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     create_table :tipo_contas do |t|
       t.string :nome, :null => false, :unique => true
       t.boolean :debito, :null => false, :default => false #se débito for false a conta é do tipo crédito, por exemplo, caixa é um tipo de conta crédito em um sistema financeiro, e "Despesas alunos" é um tipo de conta débito, um lançamento em uma conta débito é menos dinheiro em caixa
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     #contas como caixa, conta em banco, cartão de crédito, ...
@@ -38,6 +45,7 @@ class InitialModels < ActiveRecord::Migration
       t.belongs_to :empresa
       t.belongs_to :conta #conta pai, para hierarquia de contas
       t.belongs_to :tipo_conta, :null => false
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     #lançamentos dde crédito e débito futuros
@@ -48,7 +56,18 @@ class InitialModels < ActiveRecord::Migration
       t.date :fim, :null => true
       t.string :nome, :null => false
       t.text :descricao
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
+    end
+    create_table :tipo_documentos do |t|
+       t.string :nome, :null => false, :unique => true
+      t.boolean :removido, :null => false, :default => false
+      t.timestamps
+    end
+    create_table :realizacoes do |t|
+       t.belongs_to :previsao
+       t.date :data
+       t.belongs_to :lancamento
     end
     create_table :lancamento do |t|
       t.belongs_to :cliente, :null => true
@@ -56,8 +75,9 @@ class InitialModels < ActiveRecord::Migration
       t.belongs_to :origem #conta origem
       t.belongs_to :destino #conta destino
       t.belongs_to :grupo #lançamentos agrupados
-      t.belongs_to :previsao
       t.date :data, :null => false
+      t.belongs_to :tipo_documento
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
     #fechamento caixa, caixa é considerado uma conta, vai ser apenas uma sumarização de uma conta, o que também vai facilitar futuros relatórios
@@ -65,6 +85,7 @@ class InitialModels < ActiveRecord::Migration
       t.belongs_to :conta, :null => false
       t.belongs_to :usuario, :null => false
       t.date :data, :null => false
+      t.boolean :removido, :null => false, :default => false
       t.timestamps
     end
   end
@@ -72,6 +93,7 @@ class InitialModels < ActiveRecord::Migration
   def self.down
     drop_table :sumarizacao_contas
     drop_table :lancamento
+    drop_table :tipo_documentos
     drop_table :previsoes
     drop_table :contas
     drop_table :tipo_contas
